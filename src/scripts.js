4b27924b8f33194993c71c2cf7ea00a165bd76b8
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-08-02 22:19:38 -0600

    Add button functionality to HTML skeleton

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,61 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
+
+//query selectors
+let bookingsButton = document.getElementById('yourBookingsButton')
+let yourTransactionsButton = document.getElementById('yourTransactionsButton')
+let bookARoomButton = document.getElementById('bookARoomButton')
+let homeSection = document.getElementById('homeSection')
+let bookingsSection = document.getElementById('bookingsSection')
+let transactionsSection = document.getElementById('transactionsSection')
+let bookARoomSection = document.getElementById('bookARoomSection')
+let homeButton = document.getElementById('homeButton')
+
+//event listeners
+bookingsButton.addEventListener('click', showBookingsSection);
+yourTransactionsButton.addEventListener('click', showTransactionsSection);
+bookARoomButton.addEventListener('click', showBookARoomSection);
+homeButton.addEventListener('click', goHome);
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+function goHome() {
+    hide(homeButton)
+    show(homeSection);
+    hide(transactionsSection);
+    hide(bookingsSection);
+    hide(bookARoomSection)  
+}
+function showBookingsSection () {
+    hide(homeSection);
+    show(bookingsSection)
+    hide(transactionsSection);
+    show(homeButton)
+}
+
+
+function showTransactionsSection () {
+    hide(homeSection);
+    hide(bookingsSection);
+    show(transactionsSection)   
+    show(homeButton)
+
+}
+
+function showBookARoomSection () {
+    hide(homeSection);
+    show(bookARoomSection)  
+    hide(transactionsSection);
+    show(homeButton)
+
+}
+
+function show(event) {
+    event.classList.remove('hidden')
+  };
+  
+function hide(event) {
+    
+    event.classList.add('hidden')
+  };
