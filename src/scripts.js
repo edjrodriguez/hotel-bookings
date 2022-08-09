354123b5e669b9e57dd5e69737ed8ff10f5068b8
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-08-09 14:08:59 -0600

    Refactor styles for  availble rooms section and bookings section

--- src/scripts.js
+++ src/scripts.js
@@ -53,12 +53,18 @@ collapsibleBookingsMenu[i].addEventListener('click', function() {
     this.classList.toggle("active");
     let bookingsHistory = this.nextElementSibling;
     if(bookingsHistory.style.display === 'block') {
+        collapsibleBookingsMenu[0].innerText = "Show Your Bookings"
         bookingsHistory.style.display = "none";
     } else {
+        collapsibleBookingsMenu[0].innerText = "Hide Your Bookings"
         bookingsHistory.style.display = "block";
     }
 })
 }
+
+
+
+
 
 //event listeners
 // bookingsButton.addEventListener('click', showBookingsSection);
@@ -89,7 +95,12 @@ roomTypeSelect.addEventListener('change', filterByType)
 //     })
 // }
 
+function resetRoomTypeSelect() {
+    roomTypeSelect.selectedIndex = 0;                                      
+}
+
 function displayCustomerName(customerNumber) {
+
     currentCustomer = hotel.customers[2]
     userNameWelcome.innerHTML = `<p class="user-name" id="userNameWelcome"> Welcome, ${currentCustomer.name}</p> 
     <p class="user-amount-spent-on-rooms" id="userHotelTotals">Total amount spent on rooms: $${hotel.customerBillingStatments(currentCustomer)}</p>
@@ -185,11 +196,14 @@ function confirmBooking() {
 }
 
 function backToMain() {
+    resetRoomTypeSelect()
     hide(bookingConfirmedMessage)
     show(bookARoomButton)
+    hide(displayAvailableRoomsForSelectedDate)
 }
 
 function newSearch(event) {
+    resetRoomTypeSelect()
     hide(displayAvailableRoomsForSelectedDate)
     pickADate.innerHTML =     
     ` <h1 class="pick-a-date" id="pickADate">Pick a date to book your room 
@@ -212,14 +226,14 @@ function renderAvailableRooms(selectedDate) {
     hotel.notAvailableRoomNumbers = [];
     numberOfRoomsAvailable.innerHTML = " ";
     // bookARoomSection.innerText = " "
-    pickADate.innerHTML = `<h1 class="showing-rooms-by-date" id="showingRoomsByDate">Showing rooms for ${selectedDate} <button id="searchNewDate">Search New Date</button></h1>`
-    numberOfRoomsAvailable.innerHTML += `<p>Total avaialble rooms: ${availableRoomsByDate.length}</p>`
+    pickADate.innerHTML = `<h1 class="showing-rooms-by-date" id="showingRoomsByDate">Showing rooms for ${selectedDate} <button class="search-new-date-btn "id="searchNewDate">Search New Date</button></h1>`
+    numberOfRoomsAvailable.innerHTML += `<p>Total available rooms: ${availableRoomsByDate.length}</p>`
     availableRoomsByDate.forEach(availableRoom => {
         availableRoomsDisplay.innerHTML +=  `<div class="available-room-container">
-        <h4 class="available-room-type">Type of Room: ${availableRoom.roomType}</h4>
-        <h4 class="available-room-cost"> Cost Per Night: ${availableRoom.costPerNight}</h4>
+        <h4 class="available-room-type">Type of Room: ${(availableRoom.roomType.toUpperCase())}</h4>
+        <h4 class="available-room-cost"> Cost Per Night: $${availableRoom.costPerNight.toFixed(2)}</h4>
         <ul class="available-room-details">
-            <li>Bed Size: ${availableRoom.bedSize}  </li>
+            <li>Bed Size: ${availableRoom.bedSize.toUpperCase()}  </li>
             <li>Number of Beds: ${availableRoom.numBeds}   </li>
             <li>Has bidet: ${availableRoom.bidet}   </li>
             <li class="hidden"> ${availableRoom.number} </li>
@@ -238,16 +252,16 @@ function filterByType(event) {
         if(roomsByType.length === 0){
             numberOfRoomsAvailable.innerHTML = `<p>Total available ${event.target.value}s: ${roomsByType.length}</p>`
             availableRoomsDisplay.innerHTML = " "
-            availableRoomsDisplay.innerHTML += `<h4 class="cannot-find-room">${"We apologize but this room type is not availble for your selected date.  Please pick a different room type or a different date"}</h4>`
+            availableRoomsDisplay.innerHTML += `<h4 class="cannot-find-room">We apologize but this room type is not available for your selected date.</h4> <h5 class="cannot-find-room-more-details"> Please pick a different room type or a different date.</h5>`
          } else {
             availableRoomsDisplay.innerHTML = " "
             numberOfRoomsAvailable.innerHTML = `<p>Total available ${event.target.value}s: ${roomsByType.length}</p>`
              roomsByType.forEach(room => {
              availableRoomsDisplay.innerHTML +=  `<div class="available-room-container">
-                 <h4 class="available-room-type">Type of Room: ${room.type}</h4>
-                 <h4 class="available-room-cost"> Cost Per Night: ${room.costPerNight}</h4>
+                 <h4 class="available-room-type">Type of Room: ${room.roomType.toUpperCase()}</h4>
+                 <h4 class="available-room-cost"> Cost Per Night: $${room.costPerNight.toFixed(2)}</h4>
                  <ul class="available-room-details">
-                    <li>Bed Size: ${room.bedSize}  </li>
+                    <li>Bed Size: ${room.bedSize.toUpperCase()}  </li>
                     <li>Number of Beds: ${room.numBeds}   </li>
                     <li> Has bidet: ${room.bidet}   </li>
                      <li class="hidden"> ${room.number} </li>
